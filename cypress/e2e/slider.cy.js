761bc120fbf91d8867e54b392abd1a573c172e78
Jerzy Repelowicz <jeryrep@gmail.com>
2024-03-24 15:27:01 +0100

    Update packages, write new test

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,4 +1,4 @@
-describe('Swiper Gallery Test', function () {
+describe('Swiper Gallery Test', () => {
   it('Checks if second slide contains "United Kingdom"', function () {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
@@ -6,13 +6,26 @@ describe('Swiper Gallery Test', function () {
   });
 });
 
-describe('Swiper Gallery Test', function () {
-  it('Checks if third slide contains "Paris"', function () {
+describe('Swiper Gallery Test', () => {
+  it('Checks if third slide contains "Paris"', () => {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000);
     cy.get('.swiper-button-next').click({ force: true });
     cy.wait(2000);
     cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+
+describe('Swiper Gallery Test', () => {
+  it('Checks if all slides contains correct information', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').should('contain', 'Rome').and('contain', 'Italy');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom').and('contain', 'London');
+    cy.get('.swiper-button-next').click({ force: true});
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris').and('contain', 'France');
   });
 });
